78ac4445a23dd2a557624694f9792c995fc9e01a
khanh.le <lequockhanh292@gmail.com>
2024-09-05 16:28:51 +0800

    feat: add test authen, logout, add logout, test heath, apikey add authen middleware

---
+++ tests/auth/authentication/unit.test.ts
@@ -0,0 +1,72 @@
+import '@tests/database/mock';
+
+import {
+	ACCESS_TOKEN,
+	addHeaders,
+	addAuthHeaders,
+	mockUserFindById,
+	mockJwtValidate,
+	mockKeystoreFindForKey,
+	getAccessTokenSpy,
+} from './mock';
+
+import app from '@app';
+import supertest from 'supertest';
+
+describe('authentication validation', () => {
+	const endpoint = '/api/v1/health/check';
+	const request = supertest(app);
+
+	beforeEach(() => {
+		getAccessTokenSpy.mockClear();
+		mockJwtValidate.mockClear();
+		mockUserFindById.mockClear();
+		mockKeystoreFindForKey.mockClear();
+	});
+
+	it('Should response with 400 if Authorization header is not passed', async () => {
+		const response = await addHeaders(request.get(endpoint));
+		expect(response.status).toBe(400);
+		expect(response.body.message).toMatch(/authorization/);
+		expect(getAccessTokenSpy).not.toHaveBeenCalled();
+	});
+
+	it('Should response with 400 if Authorization header do not have Bearer', async () => {
+		const response = await addHeaders(request.get(endpoint)).set(
+			'Authorization',
+			'123',
+		);
+		expect(response.status).toBe(400);
+		expect(response.body.message).toMatch(/authorization/);
+		expect(getAccessTokenSpy).not.toHaveBeenCalled();
+	});
+
+	it('Should response with 401 if wrong Authorization header is provided', async () => {
+		const response = await addHeaders(request.get(endpoint)).set(
+			'Authorization',
+			'Bearer 123',
+		);
+		expect(response.status).toBe(401);
+		expect(response.body.message).toMatch(/token/i);
+		expect(getAccessTokenSpy).toHaveBeenCalledTimes(1);
+		expect(getAccessTokenSpy).toHaveBeenCalledWith('Bearer 123');
+		expect(getAccessTokenSpy).toHaveReturnedWith('123');
+		expect(mockJwtValidate).toHaveBeenCalledTimes(1);
+		expect(mockJwtValidate).toHaveBeenCalledWith('123');
+		expect(mockUserFindById).not.toHaveBeenCalled();
+	});
+
+	it('Should response with 404 if correct Authorization header is provided', async () => {
+		const response = await addAuthHeaders(request.get(endpoint));
+		expect(response.body.message).not.toMatch(/not registered/);
+		expect(response.body.message).not.toMatch(/token/i);
+		expect(response.status).toBe(404);
+		expect(getAccessTokenSpy).toHaveBeenCalledTimes(1);
+		expect(getAccessTokenSpy).toHaveBeenCalledWith(`Bearer ${ACCESS_TOKEN}`);
+		expect(getAccessTokenSpy).toHaveReturnedWith(ACCESS_TOKEN);
+		expect(mockJwtValidate).toHaveBeenCalledTimes(1);
+		expect(mockJwtValidate).toHaveBeenCalledWith(ACCESS_TOKEN);
+		expect(mockUserFindById).toHaveBeenCalledTimes(1);
+		expect(mockKeystoreFindForKey).toHaveBeenCalledTimes(1);
+	});
+});

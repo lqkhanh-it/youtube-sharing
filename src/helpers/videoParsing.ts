d65f860b9a1b21e41f208b86da1006e15c5808d5
khanh.le <lequockhanh292@gmail.com>
2024-09-08 11:28:01 +0800

    fix: deduplicate data stream, config swa deploy

--- src/helpers/videoParsing.ts
+++ src/helpers/videoParsing.ts
@@ -2,12 +2,18 @@ import fetch from 'node-fetch';
 
 // parse the video description and title from youtube url
 export const parseUrl = async (url: string) => {
-	if (!url.includes('youtube.com')) throw new Error('Not a youtube url');
+	try {
+		if (!url.includes('youtube.com')) throw new Error('Not a youtube url');
 
-	// fetch the page contents
-	const body = await fetch(url).then((res) => res.text());
+		// fetch the page contents
+		const body = await fetch(url).then((res) => res.text());
+		console.log(body);
 
-	return parseDataFromHTML(body);
+		return parseDataFromHTML(body);
+	} catch (error) {
+		console.error(error);
+		throw new Error('Unable to parse the video title and description');
+	}
 };
 
 export const parseDataFromHTML = (body: string) => {
@@ -15,6 +21,8 @@ export const parseDataFromHTML = (body: string) => {
 	const matchDesc = body.match(/<meta name="description" content="([^"]*)"/); // regular expression to parse contents of the <meta> tag
 	const thumbnail =
 		body.match(/<meta property="og:image" content="([^"]*)"/) ?? [];
+
+	console.log('@@', matchTitle, matchDesc, thumbnail);
 
 	if (!matchTitle || !matchDesc)
 		throw new Error('Unable to parse the video title and description');
